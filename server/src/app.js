3664fc55efabe4a745ff2f7f9f66b6ad7ab41e6b
Alex Voloshin <avoloshin225@gmail.com>
2018-11-01 00:15:25 +0200

    add SQLite support

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+const config = require('../config/config.js');
+const router = require('./routes');
+const {sequelize} = require('./models');
 
 const app = express();
 
@@ -9,20 +12,17 @@ app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/status', (req,res) => {
-    res.send({it:"works"})
-});
+router(app);
 
-
-app.post('/register', (req,res) => {
-    res.send({
-        message: `Hello ${req.body.email}! Your user has registered! Have fun!`,
-    })
-})
 
 const PORT = 8088 || process.env.PORT;
 
-app.listen(PORT, () => {
-    console.log(`Listening to the port ${PORT}`);
-    
-});
+sequelize.sync()
+    .then(() => {
+        app.listen(config.port, () => {
+            console.log(`Listening to the port ${PORT}`);
+            
+        });
+        
+    })
+
